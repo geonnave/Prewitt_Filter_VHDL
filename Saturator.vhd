6cd1057548ce1a39f0ac2ce02eb620c82328a34c
unknown <Geovane@Geovane-PC.(none)>
2011-12-21 16:14:56 -0200

    saturator circuit created and tested

--- Saturator.vhd
+++ Saturator.vhd
@@ -13,6 +13,7 @@ end entity;
 architecture sature of Saturator is
 begin
 	o <= "000000000" when a(17) = '1' else
+--		 "011111111" when a(17 downto 8) = '1' else
 		 "011111111" when a(8) = '1' or a(9) = '1' or a(10) = '1' or a(11) = '1' or a(12) = '1' or a(13) = '1' or 
 						  a(14) = '1' or a(15) = '1' or a(16) = '1' or a(17) = '1' else
 		 a(8 downto 0);
